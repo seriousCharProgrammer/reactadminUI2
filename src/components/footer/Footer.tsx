948ee01480bae61cd6b5558de028129342689685
seriousCharProgrammer <charbelalhajj81@gmail.com>
2025-03-26 18:49:05 +0200

    wip

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,12 @@
-import "./footer.scss"
+import './footer.scss';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+    <div className='footer'>
+      <span>CharbelAdmin</span>
+      <span>© Charbel Dev Admin Dashboard</span>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
